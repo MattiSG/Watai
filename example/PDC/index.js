9858458eebfb9a7588f5704bd957cfe707b591e1
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-07 14:32:59 +0200

    Added crash handling with browser exit.

--- example/PDC/index.js
+++ example/PDC/index.js
@@ -1,5 +1,3 @@
-
-
 var webdriver = require('/Users/eurogiciel/Documents/Ghost/selenium/build/javascript/webdriver/webdriver'),
 	growl = require('growl'),
 	config = require('./config');
@@ -16,20 +14,24 @@ driver.get(config.baseURL)
 var TR = require('../../src/TestRight.js')(driver);
 var loginWidget = require('./LoginWidget')(TR);
 
-
 driver.get(config.baseURL).then(function() {
-	loginWidget.open();
-	loginWidget.login('toto@toto.com', 'tototo');
-	
-	driver.getTitle().then(function(title) {
-		if (title.contains("Polytech'Move")) {
-			console.log(loginWidget.name + ' works');
-			growl('Success!');
-		} else {
-			console.log(loginWidget.name + ' fails');
-			growl('Fail!', { priority: 4 });
-		}
-	});
-	
-	driver.quit();
+	try {
+		loginWidget.open();
+		loginWidget.login('toto@toto.com', 'tototo');
+		
+		driver.getTitle().then(function(title) {
+			if (title.contains("Polytech'Move")) {
+				console.log(loginWidget.name + ' works');
+				growl('Success!');
+			} else {
+				console.log(loginWidget.name + ' fails');
+				growl('Fail!', { priority: 4 });
+			}
+		});
+	} catch (error) {
+		growl('Crash!\n' + error, { priority: 4 });
+		throw error;
+	} finally {
+		driver.quit();
+	}
 });
