4d0dfac9308e0db51904fa15fe91566a313ab6aa
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-12 15:56:30 +0200

    Documented Hook.

--- src/model/Hook.js
+++ src/model/Hook.js
@@ -1,36 +1,7 @@
 var webdriver = require('/Users/eurogiciel/Documents/Ghost/selenium/build/javascript/webdriver/webdriver');
 
 
-/** Adds a getter and a setter to the given Object, allowing access to the Selenium element corresponding to the given hook description.
-* The getter dynamically retrieves the Selenium element pointed at by the given selector description.
-* The setter will pass the value to the Hook.handleInput method.
-*
-*@param	target	The Object to which the getter and setter will be added.
-*@param	key	The name of the property to add to the target object.
-*@param	typeAndSelector	A hook descriptor, as defined in the Hook constructor.
-*@param	driver	The WebDriver instance in which the described elements are to be sought.
-*
-*@see	Hook
-*@see	Hook#handleInput
-*/
-exports.addHook = function addHook(target, key, typeAndSelector, driver) {
-	var hook = new Hook(typeAndSelector, driver);
-	target.__defineGetter__(key, function() {
-		if (VERBOSE)
-			console.log((target.name || '') + ' accessed ' + key);
-			
-		return hook.toSeleniumElement(hook);
-	});
-	target.__defineSetter__(key, function(input) {
-		if (VERBOSE)
-			console.log((target.name || '') + ' set ' + key + ' to "' + input + '"');
-
-		hook.handleInput(input);
-	});
-}
-
-
-/** A Hook allows one to target a specific element on a web page.
+/**@class	A Hook allows one to target a specific element on a web page.
 * It is a wrapper around both a selector and its type (css, xpath, id…).
 *
 *@param	hook	A single value-pair hash whose key may be one of `css`, `id`, or any other value of Selenium's `By` class; and whose value must be a string of the matching form.
@@ -54,8 +25,40 @@ var Hook = function Hook(hook, driver) {
 	*
 	*@param	input	A string that will be sent to this element.
 	*@see	http://seleniumhq.org/docs/03_webdriver.html#sendKeys
+	*
+	*@private
 	*/
 	this.handleInput = function handleInput(input) {
 		this.toSeleniumElement().sendKeys(input);
 	}
 }
+
+/** Adds a getter and a setter to the given Object, allowing access to the Selenium element corresponding to the given hook description.
+* The getter dynamically retrieves the Selenium element pointed at by the given selector description.
+* The setter will pass the value to the Hook.handleInput method.
+*
+*@param	target	The Object to which the getter and setter will be added.
+*@param	key	The name of the property to add to the target object.
+*@param	typeAndSelector	A hook descriptor, as defined in the Hook constructor.
+*@param	driver	The WebDriver instance in which the described elements are to be sought.
+*
+*@see	Hook
+*@see	Hook#handleInput
+*/
+Hook.addHook = function addHook(target, key, typeAndSelector, driver) {
+	var hook = new Hook(typeAndSelector, driver);
+	target.__defineGetter__(key, function() {
+		if (VERBOSE)
+			console.log((target.name || '') + ' accessed ' + key);
+			
+		return hook.toSeleniumElement(hook);
+	});
+	target.__defineSetter__(key, function(input) {
+		if (VERBOSE)
+			console.log((target.name || '') + ' set ' + key + ' to "' + input + '"');
+
+		hook.handleInput(input);
+	});
+}
+
+exports.addHook = Hook.addHook;	// CommonJS export
