e5e664b9d6f05defced609487489fcee8d05d84c
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-12 16:24:57 +0200

    Documented CLI main script.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var TR = require('./TestRight')();
 
 
-/** The main execution script takes paths to test description folders.
-*
-*@see	DescriptionLoader
-*/
+// =========================== //
+// =========== CLI =========== //
+// =========================== //
+
 var args = process.argv.slice(2); // extract CLI arguments, see http://docs.nodejitsu.com/articles/command-line/how-to-parse-command-line-arguments
 
 if (args.length == 0) {
@@ -12,17 +12,35 @@ if (args.length == 0) {
 	return 2;
 }
 
-var suites = new Array(args.length);
+main(args);
 
-args.forEach(function(descriptionPath) {
-	suites.push(new TR.SuiteLoader(descriptionPath));
-});
-
-suites.forEach(function(suite) {
-	suite.run();
-});
+// =========================== //
+// =========== /CLI ========== //
+// =========================== //
 
 
+
+/** The CLI takes paths to test description folders as arguments.
+* Any number of paths may be given.
+*
+*@param	{Array.<String>}	This **function**, as opposed to the CLI, takes in an array of paths. The CLI takes varargs.
+*
+*@see	SuiteLoader
+*/
+function main(folders) {
+	var suites = new Array(folders.length);
+	
+	folders.forEach(function(descriptionPath) {
+		suites.push(new TR.SuiteLoader(descriptionPath));
+	});
+	
+	suites.forEach(function(suite) {
+		suite.run();
+	});
+}
+
+/** Prints CLI synopsis.
+*/
 function showHelp() {
 	console.log("Usage: " + process.argv[1] + " path/to/description/folder [another [yetAnother […]]]");
 }
