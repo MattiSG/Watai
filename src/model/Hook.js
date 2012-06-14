385028fcd92c9e473985a3ced3dcde48b2929643
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-14 13:47:19 +0200

    Improved hooks access logging. Looks more and more like a scenario  :)

--- src/model/Hook.js
+++ src/model/Hook.js
@@ -49,13 +49,13 @@ Hook.addHook = function addHook(target, key, typeAndSelector, driver) {
 	var hook = new Hook(typeAndSelector, driver);
 	target.__defineGetter__(key, function() {
 		if (VERBOSE)
-			console.log('	' + (target.name || '') + ' accessed ' + key);
+			console.log('	- accessed ' + target.name + '’s ' + key);
 			
 		return hook.toSeleniumElement(hook);
 	});
 	target.__defineSetter__(key, function(input) {
 		if (VERBOSE)
-			console.log('	' + (target.name || '') + ' set ' + key + ' to "' + input + '"');
+			console.log('	- set ' + target.name + '’s ' + key + ' to “' + input + '”');
 
 		hook.handleInput(input);
 	});
