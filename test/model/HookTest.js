83b0c950989412b58e521468b7577c641c44c6e2
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-19 10:08:17 +0200

    Added first test: Hook#id.

--- test/model/HookTest.js
+++ test/model/HookTest.js
@@ -1,8 +1,17 @@
+var Hook = require('../../src/model/Hook');
+
+
 require('../helpers').test('model/Hook', function() {
 	var hooksTarget = {};
 	
 	describe('selectors', function() {
 		it('should support ID selection', function() {
+			var hookName = 'id';
+			
+			Hook.addHook(hooksTarget, hookName, { id: 'toto' }, driver);
+			
+			hooksTarget.should.have.property(hookName);
+			hooksTarget[hookName].should.be.a('object');
 		});
 	});
 });
