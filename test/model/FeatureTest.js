872b94cc364b14abdee4c0ecfdd97c06ef86ef5a
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-19 16:16:06 +0200

    Added closures evaluation tests for Feature.

--- test/model/FeatureTest.js
+++ test/model/FeatureTest.js
@@ -5,7 +5,38 @@ var WidgetTest = require('./WidgetTest');
 * It relies on some external setup, see `test/helpers` and `test/index.js`.
 */
 describe('Feature', function() {
-	var subject = new TestRight.Feature('Test feature', {
-		//TODO
-	}, [ WidgetTest.testWidget ]);
+	var featureWithScenario = function featureWithScenario(scenario) {
+		return new TestRight.Feature('Test feature', scenario, [ WidgetTest.testWidget ]);
+	}
+
+	var failingFeatureReason = 'It’s a trap!';
+	var failingFeatureTest = function() {
+		return featureWithScenario([
+			function() { throw failingFeatureReason }
+		]).test();
+	}
+	
+	describe('evaluation', function() {
+		it('of an empty feature should be accepted', function(done) {
+			featureWithScenario([]).test().then(done);
+		});
+
+		it('of a failing function should be rejected', function(done) {
+			failingFeatureTest().then(function() {
+				should.fail();
+			}, function() {
+				done();	// can't pass it directly, Mocha complains about params not being an error…
+			});
+		});
+		
+		it('of a failing function should be rejected with reasons passed', function(done) {
+			failingFeatureTest().then(function() {
+				should.fail();
+			}, function(reasons) {
+				reasons.should.have.length(1);
+				reasons[0].should.equal(failingFeatureReason);
+				done();
+			});
+		});
+	});
 });
