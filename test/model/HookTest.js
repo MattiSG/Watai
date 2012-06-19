e9d14a4f0dd68978f167c7528ac5d45e432f40db
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-06-19 10:29:27 +0200

    Improved hook getter test.

--- test/model/HookTest.js
+++ test/model/HookTest.js
@@ -5,13 +5,29 @@ require('../helpers').test('model/Hook', function() {
 	var hooksTarget = {};
 	
 	describe('selectors', function() {
-		it('should support ID selection', function() {
+		var checkHook = function checkHook(hookName, expectedContent) {
+			it('should add a hook to the target object', function() {
+				hooksTarget.should.have.property(hookName);
+			});
+			
+			it('should return an object when accessed', function() {
+				hooksTarget[hookName].should.be.a('object');
+			});
+			
+			it('should have the correct text in the retrieved element', function(done) {
+				hooksTarget[hookName].getText().then(function(content) {
+					content.should.equal(expectedContent);
+					done();
+				});
+			});
+		}
+		
+		describe('ID selection', function() {
 			var hookName = 'id';
 			
 			Hook.addHook(hooksTarget, hookName, { id: 'toto' }, driver);
 			
-			hooksTarget.should.have.property(hookName);
-			hooksTarget[hookName].should.be.a('object');
+			checkHook(hookName, 'This paragraph has id toto');
 		});
 	});
 });
