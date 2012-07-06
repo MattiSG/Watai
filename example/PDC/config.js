a4d1827f68592d0a035e191feda56dd9ac4fe9ab
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-07-06 17:25:01 +0200

    Simplified examples config.

--- example/PDC/config.js
+++ example/PDC/config.js
@@ -3,11 +3,7 @@ module.exports = {
 	
 	driverCapabilities: {
 		browserName: 'firefox',
-		version: '',
-		platform: 'ANY',
-		javascriptEnabled: true,
-		firefox_binary: '/Applications/Browsers/Firefox-headless.app/Contents/MacOS/firefox',
-		'chrome.binary': '/Applications/Browsers/Google Chrome.app/Contents/MacOS/Google Chrome'
+		javascriptEnabled: true
 	}
 }
 	
