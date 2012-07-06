68ff1335098a711b2fdc18b6fd1a5d45ba2de59a
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-07-06 10:39:46 +0200

    Removed last hardcoded path.

--- test/config.js
+++ test/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	connectURL: 'http://127.0.0.1:4444/wd/hub',
 	
-	supportPageURL: 'file:///Users/eurogiciel/Documents/Watai/test/resources/page.html',
+	supportPageURL: 'file://' + __dirname + '/resources/page.html',
 	
 	driverCapabilities: {
 		browserName: 'chrome',
