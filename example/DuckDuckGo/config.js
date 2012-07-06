89390ea03f7e9a105868ca1713864e96ed688abc
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-07-06 17:14:26 +0200

    Bumped to version 0.1.0-alpha. Getting ready for private release. Simplified examples config.

--- example/DuckDuckGo/config.js
+++ example/DuckDuckGo/config.js
@@ -2,11 +2,6 @@ module.exports = {
 	baseURL: 'https://duckduckgo.com/',
 	
 	driverCapabilities: {
-		browserName: 'chrome',
-		version: '',
-		platform: 'ANY',
-		javascriptEnabled: true,
-		firefox_binary: '/Applications/Browsers/Firefox-headless.app/Contents/MacOS/firefox',
-		'chrome.binary': '/Applications/Browsers/Google Chrome.app/Contents/MacOS/Google Chrome'
+		browserName: 'chrome'
 	}
 }
