5b1dd6c9ce66978588027dd87f6362e7816ce6fc
Matti Schneider <matti.schneider@eurogiciel.fr>
2012-07-09 12:01:38 +0200

    Automatically creating log folder on startup. Avoids failing on clone.

--- src/TestRight.js
+++ src/TestRight.js
@@ -1,37 +1,22 @@
 /* This library depends on [MooTools 1.4+](http://mootools.net). */
-var MooTools = require('mootools');
-			 	require('./lib/mootools-additions');
+require('mootools');
+require('./lib/mootools-additions');
+
 /* Logging is done with [Winston](https://github.com/flatiron/winston). */
-var winston = require('winston');
+var winston = require('winston'),
+	pathsUtils = require('path');
 
 
-/** The `suites` logger logs suites names and feature status.
-*/
-winston.loggers.add('suites', {
-	console: {
-		level: process.env.COVERAGE	// if we're computing test coverage, we can't use standard output at all, since the coverage analysis result is piped through it. The trigger is an env variable. See build automation script.
-			   ? 'error'
-			   : 'silly',
-		colorize: 'true'
-	},
-	file: {
-		filename: 'log/execution.log'
-	}
-});
+var LOG_DIR = 'log';
 
-/** The `steps` logger logs atomic actions on widgets.
-*/
-winston.loggers.add('steps', {
-	console: {
-		level: process.env.COVERAGE	// if we're computing test coverage, we can't use standard output at all, since the coverage analysis result is piped through it. The trigger is an env variable. See build automation script.
-			   ? 'error'
-			   : 'warn',
-		colorize: 'true'
-	},
-	file: {
-		filename: 'log/execution.log'
-	}
-});
+var err;
+try {
+	require('fs').mkdirSync(LOG_DIR, '0755');	// Winston cannot create the folder hierarchy on its own
+} catch (e) {
+	err = e;
+} finally {
+	initLoggers(err);
+}
 
 
 /**@namespace	This module simply exports all public classes, letting you namespace them as you wish.
@@ -55,3 +40,37 @@ var TestRight = {
 }
 
 module.exports = TestRight;	// CommonJS export
+
+
+function initLoggers(err) {
+	if (err && err.code != 'EEXIST') throw err;	// an already-existing log directory is expected and not a problem
+	
+	
+	/** The `suites` logger logs suites names and feature status.
+	*/
+	winston.loggers.add('suites', {
+		console: {
+			level: process.env.npm_config_coverage	// if we're computing test coverage, we can't use standard output at all, since the coverage analysis result is piped through it. The trigger is an env variable. See build automation script.
+				   ? 'error'
+				   : 'silly',
+			colorize: 'true'
+		},
+		file: {
+			filename: pathsUtils.join(LOG_DIR, 'execution.log')
+		}
+	});
+	
+	/** The `steps` logger logs atomic actions on widgets.
+	*/
+	winston.loggers.add('steps', {
+		console: {
+			level: process.env.npm_config_coverage	// if we're computing test coverage, we can't use standard output at all, since the coverage analysis result is piped through it. The trigger is an env variable. See build automation script.
+				   ? 'error'
+				   : 'warn',
+			colorize: 'true'
+		},
+		file: {
+			filename: pathsUtils.join(LOG_DIR, 'execution.log')
+		}
+	});
+}
