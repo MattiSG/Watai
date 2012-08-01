MattiSG/Watai

commit e420c7196501485384d3f8b62eaf66047e1e8d70

Added install check. Minor CLI refactor.

src/index.js
@@ -1,25 +1,30 @@
-var TR = require('./TestRight');
-
-var logger = require('winston').loggers.get('suites');
-
-// =========================== //
-// =========== CLI =========== //
-// =========================== //
-
+/* Here is the main CLI entry point.
+*/
 var args = process.argv.slice(2); // extract CLI arguments, see http://docs.nodejitsu.com/articles/command-line/how-to-parse-command-line-arguments
 
 if (args.length == 0) {
 	showHelp();
 	process.exit(2);
+} else if (args[0] == '--installed') {
+	var evaluation = isInstalled();
+
+	if (evaluation === true) {
+		console.log('Watai seems to be installed properly  :)  Now get testing!');
+		process.exit(0);
+	} else {
+		console.error('**Watai has not been installed properly, please make sure you followed all installation instructions.**');
+		console.error('Main reason: ', evaluation);
+		console.error('See http://github.com/MattiSG/Watai for details');
+		process.exit(1);
+	}
+} else {
+	main(args);
 }
 
-main(args);
 
-// =========================== //
-// =========== /CLI ========== //
-// =========================== //
-
-
+/** Absolute path to the main library file.
+*/
+var MAIN_FILE = exports.MAIN_FILE = require('path').join(__dirname, 'TestRight.js');
 
 /** The CLI takes paths to test description folders as arguments.
 * Any number of paths may be given.
@@ -29,6 +34,8 @@ main(args);
 *@private
 */
 function main(folders) {
+	var TR = require(MAIN_FILE);
+
 	var suites = new Array(folders.length);
 	
 	folders.forEach(function(descriptionPath) {
@@ -40,10 +47,25 @@ function main(folders) {
 	});
 }
 
+/** Provides a quick smoke test for proper installation.
+*@return	`true` if this software has all its dependencies installed or not, the error that explains what fails otherwise.
+*@private
+*/
+function isInstalled() {
+	try {
+		require('./TestRight');
+		return true;
+	} catch (e) {
+		return e;
+	}
+}
+
 /** Prints CLI synopsis.
 *@private
 */
 function showHelp() {
+	var logger = require('winston').loggers.get('suites');
+
 	logger.error("Oops, you didn’t provide any test suite to execute!");
 	logger.info("Usage: watai path/to/suite/description/folder [another/suite [yetAnother […]]]");
 }

commit 19573b8a6468a54f4f7e35bcf0200ddd6ef4a522

Corrected unstartable CLI. Bug introduced in e420c7196501485384d3f8b62eaf66047e1e8d70.

src/index.js
@@ -2,6 +2,12 @@
 */
 var args = process.argv.slice(2); // extract CLI arguments, see http://docs.nodejitsu.com/articles/command-line/how-to-parse-command-line-arguments
 
+
+/** Absolute path to the main library file.
+*/
+var MAIN_FILE = exports.MAIN_FILE = require('path').join(__dirname, 'TestRight.js');
+
+
 if (args.length == 0) {
 	showHelp();
 	process.exit(2);
@@ -20,11 +26,6 @@ if (args.length == 0) {
 } else {
 	main(args);
 }
-
-
-/** Absolute path to the main library file.
-*/
-var MAIN_FILE = exports.MAIN_FILE = require('path').join(__dirname, 'TestRight.js');
 
 /** The CLI takes paths to test description folders as arguments.
 * Any number of paths may be given.