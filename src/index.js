5f233fafa40b9435fb516bd283a80776605731d9
Matti Schneider <hi@mattischneider.fr>
2012-10-21 18:18:44 +0200

    Switched to a lovely pattern for defining options as plugins.

--- src/index.js
+++ src/index.js
@@ -3,30 +3,61 @@
 var args = process.argv.slice(2); // extract CLI arguments, see http://docs.nodejitsu.com/articles/command-line/how-to-parse-command-line-arguments
 
 /** Absolute path to the main library file.
-*@type	{string}
+*@type	{String}
 */
 var MAIN_FILE = exports.MAIN_FILE = require('path').join(__dirname, 'TestRight.js');
+
+/** Path to the directory containing option-callable scripts.
+*@type	{String}
+*@see	#preProcessArguments
+*@private
+*/
+var LOAD_OPTIONS_FROM = './plugins/';
 
 
 if (args.length == 0) {
 	showHelp();
 	process.exit(2);
-} else if (args[0] == '--installed') {
-	var evaluation = isInstalled();
-
-	if (evaluation === true) {
-		console.log('Watai seems to be installed properly  :)  Now get testing!');
-		process.exit(0);
-	} else {
-		console.error('**Watai has not been installed properly, please make sure you followed all installation instructions.**');
-		console.error('Main reason: ' + evaluation.message + '.');
-		console.error('See http://github.com/MattiSG/Watai for details.');
-		process.exit(1);
-	}
-} else {
-	main(args);
 }
 
+args = preProcessArguments(args);
+
+main(args);
+
+
+/** Loads plugins based on any passed options.
+* More precisely: any option prefixed with `--` will be recognized as a plugin, and if a matching file is found, the corresponding argument will be removed from the passed in array.
+* The convention is for such plugins to reside in a `plugins/<plugin_name>/index.js` file, and to export a function that define their expected arguments. The corresponding number of arguments will be popped from the arguments list.
+* The loaded plugin function may return an array of values, which will be added in the resulting arguments array.
+*
+*@return	{Array}	The transformed array of arguments, rid of its options and possibly filled with plugins results.
+*@private
+*/
+function preProcessArguments(args) {
+	var result = [];
+
+	for (var i = 0; i < args.length; i++) {
+		var optionMatch = args[i].match(/^--([^-].*)/);
+
+		if (optionMatch) {
+			var pluginName = optionMatch[1];	// [1] is the value of the first capturing parentheses
+
+			var plugin = require(LOAD_OPTIONS_FROM + pluginName);
+
+			var params = args.slice(i + 1, i + 1 + plugin.length);	// extract the required arguments from the CLI
+			i += plugin.length;	// update the options pointer: params for this option will be ignored
+
+			var optionResults = plugin.apply(null, params);
+
+			if (optionResults)
+				result.push.apply(result, optionResults);	// the potential array of values returned by the plugin to the new arguments array
+		} else {	// this is not an option
+			result.push(args[i]);	// pass it without doing anything on it
+		}
+	}
+
+	return result;
+}
 
 /** The CLI takes paths to test description folders as arguments.
 * Any number of paths may be given.
@@ -47,19 +78,6 @@ function main(folders) {
 	suites.forEach(function(suite) {
 		suite.run();
 	});
-}
-
-/** Provides a quick smoke test for proper installation.
-*@return	`true` if this software has all its dependencies installed or not, the error that explains what fails otherwise.
-*@private
-*/
-function isInstalled() {
-	try {
-		require(MAIN_FILE);
-		return true;
-	} catch (e) {
-		return e;
-	}
 }
 
 /** Prints CLI synopsis.
---
+++ src/plugins/installed/index.js
@@ -0,0 +1,34 @@
+/** Provides a quick smoke test for proper installation.
+*@return	`true` if this software has all its dependencies installed or not, the error that explains what fails otherwise.
+*@private
+*/
+function isInstalled() {
+	var index = require('../../');
+	try {
+		require(index.MAIN_FILE);
+		return true;
+	} catch (e) {
+		return e;
+	}
+}
+
+
+/** Presents the user with a message telling if this software passes a quick smoke test, and gives the main reason if not.
+* Exits the running process afterwards.
+*/
+function tellUserInstallationStatus() {
+	var evaluation = isInstalled();
+
+	if (evaluation === true) {
+		console.log('Watai seems to be installed properly  :)  Now get testing!');
+		process.exit(0);
+	} else {
+		console.error('**Watai has not been installed properly, please make sure you followed all installation instructions.**');
+		console.error('Main reason: ' + evaluation.message + '.');
+		console.error('See http://github.com/MattiSG/Watai for details.');
+		process.exit(1);
+	}
+}
+
+
+module.exports = tellUserInstallationStatus;	// CommonJS export
