50d6696ff39055b6a482723fc2fa1adacaf5d505
Matti Schneider <hi@mattischneider.fr>
2012-11-11 10:28:06 +0100

    Corrected views calls to pass arguments.

--- src/controller/SuiteLoader.js
+++ src/controller/SuiteLoader.js
@@ -144,10 +144,10 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 
 	attachViewsTo: function attachViewsTo(runner) {
 		configManager.values.views.each(function(view) {
-			Object.each(require('../view/Runner' + view), function(handler, eventType) {
+			Object.each(require('../view/Runner' + view), function(handler, eventType, viewObject) {
 				runner.on(eventType, function() {
 					try {
-						handler();
+						handler.apply(viewObject, arguments);
 					} catch (err) {	// TODO: investigate why such exceptions are not thrown in the main loop
 						logger.error(err + ' (in view "' + view + '")', { error: err });
 					}
