6d78ceb6b3f509199743f68deea73fd3dade872c
Matti Schneider <hi@mattischneider.fr>
2012-11-11 11:35:32 +0100

    Updated CMT example to v0.3 syntax, and switched it to Dots view.

--- example/CheckMyTrip/3 - RegisterFeature.js
+++ example/CheckMyTrip/3 - RegisterFeature.js
@@ -2,7 +2,7 @@
 	description: 'A user should be able to ask for an account',
 	
 	scenario: [
-		RegisterWidget.register, [ firstName, lastName ],
+		RegisterWidget.register, firstName, lastName,
 		{
 			'RegisterWidget.firstName': firstName,
 			'RegisterWidget.lastName': lastName
