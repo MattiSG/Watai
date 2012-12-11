7a92bcacbd27fa9d6b7213fa4407190fcd0ce8fe
Matti Schneider <hi@mattischneider.fr>
2012-12-11 23:13:30 +0100

    Make `--help` a plugin. Exit with proper status code on `--help`.

--- src/index.js
+++ src/index.js
@@ -19,8 +19,12 @@ var LOAD_OPTIONS_FROM = './plugins/';
 
 
 if (args.length == 0) {
-	showHelp();
-	process.exit(2);
+	var logger = require('winston'),
+		showHelpAndExit = require(LOAD_OPTIONS_FROM + 'help');
+
+	logger.error("Oops, you didn’t provide any test suite to execute!");
+	
+	showHelpAndExit(2);
 }
 
 args = preProcessArguments(args);
@@ -106,14 +110,4 @@ function exit(promises) {
 	});
 
 	process.exit(0);
-}
-
-/** Prints CLI synopsis.
-*@private
-*/
-function showHelp() {
-	var logger = require('winston');
-
-	logger.error("Oops, you didn’t provide any test suite to execute!");
-	logger.info("Usage: watai path/to/suite/description/folder [another/suite [yetAnother […]]]");
 }
