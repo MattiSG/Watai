MattiSG/Watai

commit f4f52fb0269b90067bdb181c0ddc1479fa112491

Clean up logging system
Remove manually-initialized loggers
Make logging much more customizable by exposing Winston setup
Make widgets emit events

src/model/Hook.js
@@ -1,8 +1,6 @@
 var webdriver = require('selenium-webdriverjs'),
 	promises = require('q');
 
-var logger = require('winston').loggers.get('steps');
-
 
 /**@class	A Hook allows one to target a specific element on a web page.
 * It is a wrapper around both a selector and its type (css, xpath, id…).
@@ -51,10 +49,10 @@ var Hook = function Hook(hook, driver) {
 * The getter dynamically retrieves the Selenium element pointed at by the given selector description.
 * The setter will pass the value to the `Hook.handleInput` method.
 *
-*@param	target	The Object to which the getter and setter will be added.
-*@param	key	The name of the property to add to the target object.
+*@param	target			The Object to which the getter and setter will be added.
+*@param	key				The name of the property to add to the target object.
 *@param	typeAndSelector	A hook descriptor, as defined in the Hook constructor.
-*@param	driver	The WebDriver instance in which the described elements are to be sought.
+*@param	driver			The WebDriver instance in which the described elements are to be sought.
 *
 *@see	Hook
 *@see	Hook#handleInput
@@ -63,20 +61,18 @@ Hook.addHook = function addHook(target, key, typeAndSelector, driver) {
 	var hook = new Hook(typeAndSelector, driver);
 
 	target.__defineGetter__(key, function() {
+		target.emit('access', key);
 		return hook.toSeleniumElement(hook);
 	});
 
-	var inputHandler = function(input) {
-		logger.info('	- set ' + target.name + '’s ' + key + ' to “' + input + '”');
-
-		return hook.handleInput(input);
+	var inputHandler = function handleInputAndEmit(input) {
+		target.emit('action', key, 'write', [ input ]);
+		hook.handleInput(input);
 	}
 
 	target.__defineSetter__(key, inputHandler);	// legacy support; works when setting inputs without any need to wait (for example, fails on animated elements)
 
-	target['set' + key.capitalize()] = function(input) {
-		return inputHandler.bind(null, input);	// use this setter when needing setters with timeouts
-	}
+	target['set' + key.capitalize()] = inputHandler;	// use this setter when needing setters with timeouts
 }
 
 module.exports = Hook;	// CommonJS export

src/Watai.js
@@ -18,9 +18,11 @@ var config = new ConfigLoader({
 
 require('./lib/configManager').set(config);
 
-initLoggers();
+Object.each(config.log, function(setup, name) {
+	winston.loggers.add(name, setup);
+});
 
-var logger = winston.loggers.get('suites');
+var logger = winston.loggers.get('init');
 
 /* Try to load long stack traces development module.
 */
@@ -29,7 +31,7 @@ try {
 	longjohn.async_trace_limit = config.debug.asyncTraces;
 	logger.silly('Long stack traces loaded');
 } catch (e) {
-	logger.silly('No long stack traces module found');
+	logger.warn('No long stack traces module found');
 }
 
 /**@namespace	This module simply exports all public classes, letting you namespace them as you wish.
@@ -70,29 +72,3 @@ var Watai = {
 }
 
 module.exports = Watai;	// CommonJS export
-
-
-/** Initializes all different Winston loggers.
-*@memberOf	Watai
-*@see	https://github.com/flatiron/winston#working-with-multiple-loggers-in-winston
-*@private
-*/
-function initLoggers() {
-	/** The `suites` logger logs suites names and feature status.
-	*/
-	winston.loggers.add('suites', {
-		console: {
-			level: config.logLevel.suites,
-			colorize: 'true'
-		}
-	});
-
-	/** The `steps` logger logs atomic actions on widgets.
-	*/
-	winston.loggers.add('steps', {
-		console: {
-			level: config.logLevel.steps,
-			colorize: 'true'
-		}
-	});
-}

src/controller/SuiteLoader.js
@@ -2,8 +2,7 @@ var fs			= require('fs'),
 	vm			= require('vm'),
 	pathsUtils	= require('path');
 
-var logger			= require('winston').loggers.get('steps'),
-	suitesLogger	= require('winston').loggers.get('suites'),
+var logger			= require('winston').loggers.get('load'),
 	ConfigLoader	= require('mattisg.configloader');
 
 var Widget			= require('../model/Widget'),
@@ -60,7 +59,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 		var loader = new ConfigLoader({
 			from: this.path,
 			appName: 'watai',
-			observer: suitesLogger.silly
+			observer: logger.silly
 		});
 
 		var config = loader.load(SuiteLoader.paths.config);
@@ -97,7 +96,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 		result[SuiteLoader.contextGlobals.featuresList] = this.features;	// hook to pass instantiated features to this context
 		result[SuiteLoader.contextGlobals.widgetsList] = {};	// stays in the managed context, but necessary for features to have a reference to all widgets, since they are evaluated in _this_ context, not their instanciation one…
 
-		result[SuiteLoader.contextGlobals.logger] = logger.info; // this has to be passed, for simpler access, but mostly because the `console` module is not automatically loaded
+		result[SuiteLoader.contextGlobals.log] = logger.info;	// this has to be passed, for simpler access, but mostly because the `console` module is not automatically loaded
 
 		result[SuiteLoader.contextGlobals.assert] = require('assert');
 		result[SuiteLoader.contextGlobals.storage] = Object.create(null);
@@ -115,7 +114,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 	*/
 	loadAllFiles: function loadAllFiles(err, files) {
 		if (err) {
-			suitesLogger.error('Error while trying to load description files in "' + this.path + '"!', { path: this.path });
+			logger.error('Error while trying to load description files in "' + this.path + '"!', { path: this.path });
 			throw err;
 		}
 
@@ -164,7 +163,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 							this.context,
 							dataFile);
 		} catch (error) {
-			suitesLogger.error('**Error in file "' + dataFile + '"**', { path : dataFile });
+			logger.error('**Error in file "' + dataFile + '"**', { path : dataFile });	// TODO: is it really useful to add this info?
 			throw error;
 		}
 
@@ -192,7 +191,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 							this.context,
 							widgetFile);
 		} catch (error) {
-			suitesLogger.error('**Error in file "' + widgetFile + '"**', { path: widgetFile });
+			logger.error('**Error in file "' + widgetFile + '"**', { path: widgetFile });	// TODO: is it really useful to add this info?
 			throw error;
 		}
 
@@ -219,7 +218,7 @@ var SuiteLoader = new Class( /** @lends SuiteLoader# */ {
 							this.context,
 							featureFile);
 		} catch (error) {
-			suitesLogger.error('**Error in file "' + featureFile + '"**', { path: featureFile });
+			logger.error('**Error in file "' + featureFile + '"**', { path: featureFile });	// TODO: is it really useful to add this info?
 			throw error;
 		}
 
@@ -274,7 +273,7 @@ SuiteLoader.contextGlobals = {
 	featuresList:	'__features__',
 	/** The name of the offered logging function.
 	*/
-	logger:			'log',
+	log:			'log',
 	/** The name of the offered assertion library.
 	*/
 	assert:			'assert',

src/lib/cli-animator-windows.js
@@ -3,7 +3,7 @@
 */
 var WindowsCLI = {};
 
-var logger = require('winston').loggers.get('suites');
+var logger = require('winston');
 
 
 /** Presents the given information to the user.

src/model/Feature.js
@@ -1,7 +1,7 @@
 var promises = require('q'),
 	assert = require('assert');
 
-var logger = require('winston').loggers.get('steps'),
+var logger = require('winston').loggers.get('load'),
 	matchers = require('./scenario/state'),
 	steps = require('./scenario'),
 	config = require('../lib/configManager');

src/model/Widget.js
@@ -1,14 +1,16 @@
 var promises = require('q');
 
-var logger = require('winston').loggers.get('steps');
-
 var Hook = require('./Hook');
 
 
 var Widget = new Class( /** @lends Widget# */ {
+
+	Extends: require('events').EventEmitter,
+
 	/** The name of this widget.
 	* Automatically set to the name of its containing file upon parsing.
 	*@type	{String}
+	*@private
 	*/
 	name: '',
 
@@ -36,15 +38,10 @@ var Widget = new Class( /** @lends Widget# */ {
 		Object.each(values, function(method, key) {
 			widget[key] = function() {
 				var args = Array.prototype.slice.call(arguments);	// make an array of prepared arguments
+
 				return function() {
-					logger.info('	- did ' + key
-								+ (args.length > 0
-								   ? ' ' + args.join(', ')
-								   : '')
-								+ ' (within '
-								+ widget.name
-								+ ')');
-					method.apply(widget, args);
+					widget.emit('action', key, method, args);
+					return method.apply(widget, args);
 				}
 			}
 		});
@@ -53,6 +50,7 @@ var Widget = new Class( /** @lends Widget# */ {
 	/** Add magic methods on specially-formatted elements.
 	* _Example: "loginLink" makes the `loginLink` element available to the widget, but also generates the `login()` method, which automagically calls `click` on `loginLink`.
 	*
+	*@param	{String}	key	The key that should be considered for adding magic elements.
 	*@see	Widget.magic
 	*@private
 	*/
@@ -61,16 +59,19 @@ var Widget = new Class( /** @lends Widget# */ {
 
 		Object.each(Widget.magic, function(matcher, method) {
 			var matches = matcher.exec(key);
-			if (matches) {	// `exec` returns `null` if no match was found
-				var basename = matches[1],
-					type = matches[2];	// for example "Link", "Button"…
 
-				widget[basename] = function() {	// wrapping to allow immediate calls in scenario steps	//TODO: rather return an object with methods, and leave preparation for scenarios to the Widget constructor
-					return function() {	// no immediate access to avoid calling the getter, which would trigger a Selenium access
-						logger.info('	- ' + method + 'ed ' + basename + ' ' + type.toLowerCase());
+			if (! matches)	// no match, hence no magic to add
+				return;
 
-						return widget[key][method]();
-					}
+			var basename = matches[1],
+				type = matches[2];	// for example "Link", "Button"…
+
+			widget[basename] = function() {	// wrapping to allow immediate calls in scenario steps	//TODO: rather return an object with methods, and leave preparation for scenarios to the Widget constructor
+				var args = Array.prototype.slice.call(arguments);	// make an array of prepared arguments
+
+				return function() {	// no immediate access to avoid calling the getter, which would trigger a Selenium access
+					widget.emit('action', key, method, args);
+					return widget[key][method].apply(widget[key], args);
 				}
 			}
 		});

test/config.js
@@ -5,10 +5,6 @@ module.exports = {
 		browserName: 'chrome',
 		javascriptEnabled: true
 	},
-	logLevel: {
-		suites: 'none',
-		steps: 'none'
-	},
 	quit: 'never',
 	browserWarmupTimeout: 30 * 1000,	//ms
 	timeout: 500	// implicit wait for elements lookup in milliseconds, has to be lower than mocha's timeout to test for missing elements rejection

commit ca2a5e619c020569e427f50336a34bd320aa5ad0

Correct `set<element>` syntax on elements
Regression introduced in f4f52fb0269b90067

src/model/Hook.js
@@ -67,12 +67,15 @@ Hook.addHook = function addHook(target, key, typeAndSelector, driver) {
 
 	var inputHandler = function handleInputAndEmit(input) {
 		target.emit('action', key, 'write', [ input ]);
-		hook.handleInput(input);
+
+		return hook.handleInput(input);
 	}
 
 	target.__defineSetter__(key, inputHandler);	// legacy support; works when setting inputs without any need to wait (for example, fails on animated elements)
 
-	target['set' + key.capitalize()] = inputHandler;	// use this setter when needing setters with timeouts
+	target['set' + key.capitalize()] = function(input) {	// wrapping to allow call-like syntax in scenarios
+		return inputHandler.bind(null, input);	// use this setter when needing setters with timeouts
+	}
 }
 
 module.exports = Hook;	// CommonJS export