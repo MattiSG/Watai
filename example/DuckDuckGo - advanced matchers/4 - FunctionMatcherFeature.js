09fc8c99ef5c83b66d9548affb6f91bad32d2775
Matti Schneider <matti.schneider@mesh-md.fr>
2013-02-26 14:22:24 +0100

    Improve DuckDuckGo Advanced Matchers example

--- example/DuckDuckGo - advanced matchers/4 - FunctionMatcherFeature.js
+++ example/DuckDuckGo - advanced matchers/4 - FunctionMatcherFeature.js
@@ -3,10 +3,10 @@
 
 	scenario: [
 		{
-			'SearchBarWidget.field': function() {
+			'SearchBarWidget.field': function isAlwaysValid() {	// providing a good name (i.e. prefixed with `is`) for the function will allow for better reports
 				return true;	// you may immediately return a value: if it is truthy, it will be considered a pass, and a fail otherwise
 			},
-			'SearchBarWidget.field': function(searchField) {	// if you need to do some advanced matching but don't feel like writing a matcher, you may use WebDriver's access
+			'SearchBarWidget.field': function hasNoAutocompletion(searchField) {	// if you need to do some advanced matching but don't feel like writing a matcher, you may use WebDriver's access
 				return searchField.getAttribute('autocomplete').then(function(attribute) {	// notice that you should **always** `return` a promise
 					if (attribute != 'off')
 						throw 'Expected autocompletion to be off';
