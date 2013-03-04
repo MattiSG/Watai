2ecbbff03025eb83aeaaae1b8464699b3d04683f
Matti Schneider <matti.schneider@mesh-md.fr>
2013-03-04 10:22:17 +0100

    Remove spinner from Flow view, making it suitable for logging usage

--- src/view/Runner/Flow.js
+++ src/view/Runner/Flow.js
@@ -2,8 +2,8 @@
 *
 * WARNING: EXPERIMENTAL. This view is provided as a help for debugging cases, but it is not ready for actual delivery yet.
 */
-var RunnerVerbose = new Class({
-	Extends: require('./CLI'),
+var RunnerFlow = new Class({
+	Extends: require('../PromiseView'),
 
 	submodel: {
 		name: 'feature',
@@ -11,12 +11,19 @@ var RunnerVerbose = new Class({
 	},
 
 	events: {
-		/** Informs the user that the emitting Runner is ready to start.
+		/** Informs user that this view's Runner is waiting for the browser.
 		*/
-		ready: function onReady(runner) {
-			this.animator.log('➔ ', 'info', runner + '                       ');
+		driverInit: function onDriverInit() {
+			this.animator.log('➔ ', 'info', this.model);
+			process.stdout.write('    Waiting for browser… ');
+		},
+
+		/** Informs the user that this view's Runner is ready to start.
+		*/
+		ready: function onReady() {
+			process.stdout.write('ready!\n');
 		}
 	}
 });
 
-module.exports = RunnerVerbose;	// CommonJS export
+module.exports = RunnerFlow;	// CommonJS export
