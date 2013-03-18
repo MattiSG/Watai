a9fa630009795f8858a9782c715feaef37450eb1
Matti Schneider <matti.schneider@mesh-md.fr>
2013-03-18 16:47:11 +0100

    Split config files in config/setup couples
    [non-functional]

--- test/config.js
+++ test/config.js
@@ -8,11 +8,4 @@ module.exports = {
 	quit: 'never',
 	browserWarmupTimeout: 30 * 1000,	//ms
 	timeout: 500,	// implicit wait for elements lookup in milliseconds, has to be lower than mocha's timeout to test for missing elements rejection
-	log: {
-		load: {
-			console: {
-				silent: true	// we will test for errors and don't want these errors to be logged
-			}
-		}
-	},
 }
---
+++ test/setup.js
@@ -0,0 +1,9 @@
+module.exports = {
+	log: {
+		load: {
+			console: {
+				silent: true	// we will test for errors and don't want these errors to be logged
+			}
+		}
+	}
+}
