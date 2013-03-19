b2436635552a6ee52f13844143dea5d7c54ba532
Matti Schneider <matti.schneider@mesh-md.fr>
2013-03-19 11:40:37 +0100

    Test `--config` option

---
+++ test/main/optionConfigTest.js
@@ -0,0 +1,51 @@
+var spawn = require('child_process').spawn;
+
+
+var BIN = './src/index.js';
+
+
+describe('--config option', function() {
+	it('should override the config values', function(done) {
+		this.timeout(30 * 1000);
+
+		var config = {
+			baseURL: 'file://' + __dirname + '/../resources/page_with_missing_element.html'	// the strategy is to use a suite that fails with its default config, and works with the overriding options
+		};
+
+		var subject = spawn(BIN, [ '--config', JSON.stringify(config), 'test/resources/FailingSuite' ]);
+
+		subject.on('exit', function(code) {
+			code.should.equal(0);
+			done();
+		});
+	});
+
+	it('should fail if not passed any option', function(done) {
+		var subject = spawn(BIN, [ '--config', 'test/resources/FailingSuite' ]);
+
+		subject.on('exit', function(code) {
+			code.should.equal(1);
+			done();
+		});
+	});
+
+	it('should fail if passed incorrectly-formatted options', function(done) {
+		var subject = spawn(BIN, [ '--config', '{', 'test/resources/FailingSuite' ]);
+
+		subject.on('exit', function(code) {
+			code.should.equal(1);
+			done();
+		});
+	});
+
+	it('should not do anything if passed empty options', function(done) {
+		this.timeout(30 * 1000);
+
+		var subject = spawn(BIN, [ '--config', '{}', 'test/resources/FailingSuite' ]);
+
+		subject.on('exit', function(code) {
+			code.should.equal(1);
+			done();
+		});
+	});
+});
