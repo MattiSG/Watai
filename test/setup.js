7e246f5b4bf111afaa951cdb104d3fb11865036e
Matti Schneider <matti.schneider@mesh-md.fr>
2013-03-19 14:03:08 +0100

    Specify setup for tests

--- test/setup.js
+++ test/setup.js
@@ -1,9 +1,16 @@
 module.exports = {
 	log: {
+		init: {
+			console: {
+				level		: 'error',
+				colorize	: true
+			}
+		},
 		load: {
 			console: {
 				silent: true	// we will test for errors and don't want these errors to be logged
 			}
 		}
-	}
+	},
+	asyncTracesLimit: -1	// set to -1 for unlimited, 0 for none. More details at <https://github.com/mattinsler/longjohn#README>.
 }
