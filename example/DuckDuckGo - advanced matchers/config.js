91e3d64f091448b46682ff124eb2b0a5afae8318
Matti Schneider <matti.schneider@mesh-md.fr>
2013-03-21 11:04:43 +0100

    Document baseURL usage as an URL object

--- example/DuckDuckGo - advanced matchers/config.js
+++ example/DuckDuckGo - advanced matchers/config.js
@@ -1,6 +1,10 @@
 module.exports = {
-	baseURL: 'https://duckduckgo.com/?kad=en_GB',	// you don't need to add any query string to test your own website
-													// passing the language code is used only because DuckDuckGo adjusts to your default language, which may make the test fail
+	baseURL: {
+		protocol:	'https',
+		hostname:	'duckduckgo.com',
+		query:		{ kad:'en_GB' }	// passing the language code is used only because DuckDuckGo adjusts to your default language, which may make the test fail
+	},
+
 	browser: 'chrome',	// this is a shortcut, relying on chromedriver having been installed, and on browsers being in their default paths
 						// for more information, see the [documentation](https://github.com/MattiSG/Watai/wiki/Testing-with-Chrome) or check out the DuckDuckGo simple example
 	views: [ 'Flow' ]
