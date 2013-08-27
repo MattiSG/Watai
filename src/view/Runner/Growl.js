e466547cf6bb18db9c991a1ef94cd18414dc36c3
Matti Schneider <hi@mattischneider.fr>
2013-08-27 17:15:36 +0200

    Improve Growl view Add failure details Use browser icon

--- src/view/Runner/Growl.js
+++ src/view/Runner/Growl.js
@@ -13,12 +13,27 @@ try {
 var RunnerGrowl = new Class({
 	Extends: require('../PromiseView'),
 
-	showFailure: function showFailure() {
-		growl('Test failed  :(', { priority: 4 });
+	showFailure: function showFailure(reason) {
+		this.show(reason.toString(), {
+			title:		'Test failed',
+			priority:	4
+		});
 	},
 
 	showSuccess: function showSuccess() {
-		growl('Test succeeded!  :)', { priority: 3 });
+		this.show(this.model.features.length + ' features passed', {
+			title: 'Test succeeded'
+		});
+	},
+
+	show: function show(content, options) {
+		var defaults = {
+			name:		'Watai',
+			image:		this.model.config.driverCapabilities.browserName.capitalize(),
+			priority:	3
+		};
+
+		growl(content, Object.merge(defaults, options));
 	}
 });
 
