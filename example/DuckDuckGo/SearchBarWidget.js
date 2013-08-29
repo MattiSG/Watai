6dd008220620358ada18ccfddb0f8ef3441b6cd8
Matti Schneider <hi@mattischneider.fr>
2013-08-29 19:02:17 +0200

    Make DuckDuckGo example more canonical

--- example/DuckDuckGo/SearchBarWidget.js
+++ example/DuckDuckGo/SearchBarWidget.js
@@ -1,11 +1,11 @@
 {
 	elements: {
-		field:	{ name: 'q' }
+		field:			{ name: 'q' },
+		submitButton:	{ id: 'search_button_homepage' }
 	},
 
 	searchFor: function searchFor(term) {
-		return this.setField(term)().then(function(element) {
-			return driver.submit(element);
-		});
+		return	this.setField(term)()
+					.then(this.submit());
 	}
 }
