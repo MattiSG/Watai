605a1ab80ec67a29b50f33df7e325daf8a71f952
Matti Schneider <hi@mattischneider.fr>
2013-08-30 14:33:23 +0200

    Simplify custom functions handling thanks to WD update ! A custom function must now throw if it wants to reject a match Update example

--- example/DuckDuckGo - advanced matchers/4 - FunctionMatcherFeature.js
+++ example/DuckDuckGo - advanced matchers/4 - FunctionMatcherFeature.js
@@ -1,16 +1,17 @@
 {
-	description: 'Check an HTML attribute on an element',
+	description: 'Check an HTML attribute on an element with custom matcher functions',
 
 	scenario: [
 		{
-			'SearchBarWidget.field': function isAlwaysValid() {	// providing a good name (i.e. prefixed with `is`) for the function will allow for better reports
-				return true;	// you may immediately return a value: if it is truthy, it will be considered a pass, and a fail otherwise
+			'ZeroClickWidget.header': function isAlwaysValid(header) {	// providing a good name (i.e. prefixed with `is`) for the function will allow for better reports
+				assert.ok(true, 'This is always valid');	// you can do whatever you please in a function; if it throws, it will be considered a failure
+				// Node's [assert](http://nodejs.org/api/assert.html) default library is injected, so you can use it instead of `throw`ing yourself
 			},
-			'SearchBarWidget.field': function hasNoAutocompletion(searchField) {	// if you need to do some advanced matching but don't feel like writing a matcher, you may use WebDriver's access
-				return searchField.getAttribute('autocomplete').then(function(attribute) {	// notice that you should **always** `return` a promise
-					if (attribute != 'off')
-						throw 'Expected autocompletion to be off';
-				});
+			'SearchBarWidget.field': function hasNoAutocompletion(searchField) {	// custom matchers are always passed a WD reference to the element they evaluate; see API for such objects at <https://github.com/admc/wd#supported-methods>
+				return searchField.getAttribute('autocomplete')	// notice that if you're using a promise, you should **always** `return` from it
+						.then(function(attribute) {	// a promise-returning matcher will be evaluated based on whether it was resolved or rejected
+							assert.equal(attribute, 'off', 'Expected autocompletion to be off');
+						});
 			}
 		}
 	]
--- test/model/scenario/state/FunctionMatcherTest.js
+++ test/model/scenario/state/FunctionMatcherTest.js
@@ -5,28 +5,37 @@ var Watai = require('../../../helpers/subject'),
 
 
 describe('FunctionMatcher', function() {
+	var MESSAGE = 'This should make it fail';
+
+
 	before(function() {
 		TestWidget = require('../../../helpers/testWidget').getWidget(my.driver);
 	});
 
 	it('should pass on a function returning `true`', function(done) {
 		new FunctionMatcher(function() { return true }, 'TestWidget.toggleCheckbox', { TestWidget: TestWidget })
-			.test().then(function() {
-				done()
-			}, done).done();
+			.test()
+			.done(function() { done() }, done);
 	});
 
-	it('should fail on a function returning `false`', function(done) {
+	it('should pass on a function returning `false`', function(done) {
 		new FunctionMatcher(function() { return false }, 'TestWidget.toggleCheckbox', { TestWidget: TestWidget })
-			.test().then(function() {
+			.test()
+			.done(function() { done() }, done);
+	});
+
+	it('should fail on a throwing function', function(done) {
+		new FunctionMatcher(function() { throw MESSAGE }, 'TestWidget.toggleCheckbox', { TestWidget: TestWidget })
+			.test()
+			.done(function() {
 				done(new Error('Resolved instead of rejected!'));
 			}, function(reason) {
+				reason.should.match(new RegExp(MESSAGE));
 				done();
-			}
-		).done();
+			});
 	});
 
-	it('should pass on a function returning a promise', function(done) {
+	it('should pass on a function returning a resolved promise', function(done) {
 		new FunctionMatcher(function(elm) {
 					return elm.getAttribute('checked').then(function() {	// this is just a way to obtain a promise
 						// do nothing
@@ -34,27 +43,24 @@ describe('FunctionMatcher', function() {
 				},
 				'TestWidget.toggleCheckbox',
 				{ TestWidget: TestWidget }
-			).test().then(function() {
-				done()
-			}, done).done();
+			).test()
+			.done(function() { done() }, done);
 	});
 
-	it('should fail on a function returning a throwing promise', function(done) {
-		var expectedMessage = 'This should make it fail';
-
+	it('should fail on a function returning a rejected promise', function(done) {
 		new FunctionMatcher(function(elm) {
 					return elm.getAttribute('checked').then(function() {	// this is just a way to obtain a promise
-						throw expectedMessage;
+						throw MESSAGE;
 					});
 				},
 				'TestWidget.toggleCheckbox',
 				{ TestWidget: TestWidget }
-			).test().then(function() {
+			).test()
+			.done(function() {
 				done(new Error('Resolved instead of rejected!'));
 			}, function(reason) {
-				reason.should.match(new RegExp(expectedMessage));
+				reason.should.match(new RegExp(MESSAGE));
 				done();
-			}
-		).done();
+			});
 	});
 });
