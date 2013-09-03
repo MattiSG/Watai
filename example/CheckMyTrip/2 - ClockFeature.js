34002173a9fc1c0e6c54a53f9254ae36b30cac41
Matti Schneider <matti.schneider@mesh-md.fr>
2013-09-03 12:59:12 +0200

    Name actions in CMT example Improve CheckMyTrip report

--- example/CheckMyTrip/2 - ClockFeature.js
+++ example/CheckMyTrip/2 - ClockFeature.js
@@ -2,14 +2,14 @@
 	description: 'A user should be able to look up ' + clockLookupTown + '’s local time',
 
 	scenario: [
-		function() {
+		function storeCurrentTime() {
 			return ClockWidget.getCurrentHour()()
 							  .then(function(hour) {
 							  	storage.previousHour = hour;
 							  });
 		},
 		ClockWidget.lookup(clockLookupTown),
-		function() {
+		function hasExpectedTimeDiff() {
 			return ClockWidget.getCurrentHour()()
 							  .then(function(hour) {
 							  	assert.equal(hour, storage.previousHour - 1);
