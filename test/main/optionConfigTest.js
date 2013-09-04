937b093574cbae73fd8de238360583266d5f1f6d
Nicolas Dupont <nicolas.dupont@mesh-md.fr>
2013-09-04 11:43:25 +0200

    Use the `SucceedingSuite` in optionConfigTest which is safer.
    The condition of `should not do anything if passed empty options` test is inverted to prevent having an error code returned due to test failure.

--- test/main/optionConfigTest.js
+++ test/main/optionConfigTest.js
@@ -40,7 +40,7 @@ describe('--config option', function() {
 	});
 
 	it('should fail if not passed any option', function(done) {
-		var subject = spawn(BIN, [ '--config', 'example/DuckDuckGo' ]);
+		var subject = spawn(BIN, [ '--config', 'test/resources/SucceedingSuite' ]);
 
 		subject.on('exit', function(code) {
 			code.should.not.equal(0); // Node 0.8 returns an error code 1 and Node 0.10 returns an error code 8, for retrocompatibility we test only if the code is not a success code 0
@@ -77,10 +77,10 @@ describe('--config option', function() {
 	it('should not do anything if passed empty options', function(done) {
 		this.timeout(30 * 1000);
 
-		var subject = spawn(BIN, [ '--config', '{}', 'test/resources/FailingSuite' ]);
+		var subject = spawn(BIN, [ '--config', '{}', 'test/resources/SucceedingSuite' ]);
 
 		subject.on('exit', function(code) {
-			code.should.equal(1);
+			code.should.equal(0);
 			done();
 		});
 	});
