00757d3976df485df180cc3bfe84f290ac39a3a5
Gilles Fabio <gilles@gillesfabio.com>
2013-10-01 11:51:03 +0200

    WebSocket view: add doc.

--- src/view/Feature/WebSocket.js
+++ src/view/Feature/WebSocket.js
@@ -1,9 +1,23 @@
 'use strict';
 
-var FeatureWebSocketView = new Class({
+/**
+ * @class A work-in-progress WebSocket interface that sends Feature's events through WebSocket.
+ * WARNING: EXPERIMENTAL. This view is not ready for actual delivery yet.
+ */
+var FeatureWebSocket = new Class(/** @lends FeatureWebSocket# */{
 
 	Extends: require('../PromiseView'),
 
+	/**
+	 * Sends details of a test success.
+	 *
+	 * This will send a WebSocket message with the following properties:
+	 *
+	 *   - `type`        : Always set to `watai:websocket:feature`
+	 *   - `runDate`     : The test suite running date (when `Runner` start)
+	 *   - `status`      : Always set to `success`
+	 *   - `description` : The current model description
+	 */
 	showSuccess: function showSuccess() {
 		var self = this;
 		this.sender.emit('send', {
@@ -14,6 +28,27 @@ var FeatureWebSocketView = new Class({
 		});
 	},
 
+	/**
+	 * Sends details of a test failure.
+	 *
+	 * This will send a WebSocket message with the following properties:
+	 *
+	 *   - `type`        : Always set to `watai:websocket:feature`
+	 *   - `runDate`     : The test suite running date (when `Runner` start)
+	 *   - `status`      : Always set to `failure`
+	 *   - `description` : The current model description (with feature ID)
+	 *   - `reason`      : The failure error description
+	 *
+	 *  The `reason` property is an object with the following properties:
+	 *
+	 *   - `title`  : The error title
+	 *   - `help`   : The error help
+	 *   - `source` : The error source code
+	 *
+	 * This object is returned by `this.getErrorDescription()`.
+	 *
+	 * @param {object} reason - The failure error
+	 */
 	showFailure: function showFailure(reason) {
 		var self = this;
 		this.sender.emit('send', {
@@ -27,4 +62,4 @@ var FeatureWebSocketView = new Class({
 
 });
 
-module.exports = FeatureWebSocketView;
+module.exports = FeatureWebSocket;
--- src/view/Runner/WebSocket.js
+++ src/view/Runner/WebSocket.js
@@ -1,17 +1,25 @@
 'use strict';
 
-var http                 = require('http'),
-	WebSocketServer      = require('ws').Server,
-	FeatureWebSocketView = require('../Feature/WebSocket'),
-	wsNamespace          = 'watai:websocket:',
-	events               = require('events'),
-	sender               = new events.EventEmitter();
+var http             = require('http'),
+	WebSocketServer  = require('ws').Server,
+	FeatureWebSocket = require('../Feature/WebSocket'),
+	wsNamespace      = 'watai:websocket:',
+	EventEmitter     = require('events').EventEmitter,
+	sender           = new EventEmitter();
 
 
-var RunnerWebSocketView = new Class({
+/**
+ * @class A work-in-progress WebSocket interface that sends Runner's events through WebSocket.
+ * WARNING: EXPERIMENTAL. This view is not ready for actual delivery yet.
+ */
+var RunnerWebSocket = new Class(/** @lends RunnerWebSocket# */{
 
 	Extends: require('../PromiseView'),
 
+	/**
+	 * Initialize the view.
+	 * @param  {object} model - The model
+	 */
 	initialize: function initialize(model) {
 		this.httpServer = http.createServer();
 		this.wss        = new WebSocketServer({server: this.httpServer});
@@ -25,7 +33,22 @@ var RunnerWebSocketView = new Class({
 		this.parent(model);
 	},
 
+	/**
+	 * The view events.
+	 * @type {object}
+	 */
 	events: {
+
+		/**
+		 * Sends a WebSocket message when Runner is ready to start.
+		 *
+		 * The WebSocket message object has the following properties:
+		 *
+		 *   - `type`        : Always set to `watai:websocket:runner`
+		 *   - `action`      : Always set to `start` for this event
+		 *   - `runDate`     : The runner start date
+		 *   - `name`        : The Runner name
+		 */
 		ready: function onReady() {
 			var self = this;
 			sender.emit('send', {
@@ -35,8 +58,20 @@ var RunnerWebSocketView = new Class({
 				name    : self.model.toString()
 			});
 		},
+
+		/**
+		 * Assigns new properties to each feature.
+		 *
+		 * These properties are:
+		 *
+		 *   - `sender`      : `EventEmitter` instance with a `send` event that passes message to WebSocket's current connection
+		 *   - `wsNamespace` : The prefix to use as namespace for WebSocket message `type` property
+		 *   - `runDate`     : The test suite running date (when this runner starts)
+		 *
+		 * @param {object} feature - The feature
+		 */
 		feature: function onFeature(feature) {
-			var view         = new FeatureWebSocketView(feature);
+			var view         = new FeatureWebSocket(feature);
 			view.sender      = sender;
 			view.wsNamespace = wsNamespace;
 			view.runDate     = this.runDate;
@@ -44,4 +79,4 @@ var RunnerWebSocketView = new Class({
 	}
 });
 
-module.exports = RunnerWebSocketView;
+module.exports = RunnerWebSocket;
