94d972b941eca1b89709c3150464e49ab2134e88
Matti Schneider <hi@mattischneider.fr>
2013-11-17 12:06:23 +0100

    Refactor control flow in feature failure handling
    Prevent "[object Object]" message on failures introduced in 1cae668da due to startNextFeature returning a promise for the whole run instead of the next feature only
    Simplify control flow by having only one flow-handling method and one flagging method

--- src/controller/Runner.js
+++ src/controller/Runner.js
@@ -215,12 +215,12 @@ var Runner = new Class( /** @lends Runner# */ {
 	start: function start() {
 		this.failures = {};
 		this.currentFeature = -1;
+		this.startNextFeature();
 
-		return this.startNextFeature();
+		return this.promise;
 	},
 
 	/** Increments the feature index, starts evaluation of the next feature, and quits the driver if all features were evaluated.
-	*@returns	{QPromise}	The promise for this run to be finished.
 	*
 	*@private
 	*/
@@ -233,8 +233,6 @@ var Runner = new Class( /** @lends Runner# */ {
 			this.evaluateFeature(this.features[this.currentFeature]);
 		else
 			this.finish();
-
-		return this.promise;
 	},
 
 	/** Prepares and triggers the evaluation of the given feature.
@@ -245,20 +243,18 @@ var Runner = new Class( /** @lends Runner# */ {
 	evaluateFeature: function evaluateFeature(feature) {
 		this.emit('feature', feature);
 
-		feature.test().done(this.startNextFeature.bind(this),
-							this.handleFeatureFailure.bind(this, feature)); // leave last arg to pass failure description
+		feature.test()
+			   .fail(this.storeFailure.bind(this, feature)) // leave last arg to pass failure description
+			   .done(this.startNextFeature.bind(this));	// TODO: make startNextFeature return a promise for the next feature to be evaluated, and orchestrate flow around it with an array reduce
 	},
 
-	/** Callback handler upon feature evaluation. Flags failures and calls the `startNextFeature` handler.
+	/** Callback handler upon feature evaluation. Flags failures and prepares final error report.
 	*
 	*@private
-	*@see	#startNextFeature
 	*/
-	handleFeatureFailure: function handleFeatureFailure(feature, message) {
+	storeFailure: function storeFailure(feature, message) {
 		this.failures[feature] = message;
 		this.failed = true;
-
-		this.startNextFeature();
 	},
 
 	/** Informs of the end result and cleans the instance up after tests runs.
